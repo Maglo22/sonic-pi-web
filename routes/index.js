c1667d89760c643800f5925bf8a9e41ce3e969a0
Bruno Maglioni <br1@br1-debian.ipv6.infinitum.net.mx>
2020-04-20 12:30:23 -0500

    delete pad route related code

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var etherpad_api = require('../etherpad-api');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  etherpad_api.listAllPads(function(error, data) {
-    if(error) {
-      res.render('error', {error: error});
-    } else {
-      res.render('index', { title: 'Sonic-Pi-Web', pads: data });
-    }
-  });
+  //res.render('error', {error: error});
+  res.render('index', { title: 'Sonic-Pi-Web' });
 });
 
-/* join a pad */
-router.get('/pad/:id', function(req, res) {
-  res.render('pad', { title: 'Pad: ' + req.params.id, id: req.params.id });
-});
-
-/* create a pad */
-router.post('/pad/new/:id', function(req, res) {
-  etherpad_api.createPad(req.params.id, (error, data) => {
-    if (error) {
-      res.render('error', {error: error});
-    } else {
-      res.redirect('/');
-    }
-  });
-});
-
-/* delete a pad */
-router.get('/pad/delete/:id', function(req, res) {
-  etherpad_api.deletePad(req.params.id, (error, data) => {
-    if (error) {
-      res.render('error', {error: error});
-    } else {
-      res.redirect('/');
-    }
-  });
-});
 
 module.exports = router;
