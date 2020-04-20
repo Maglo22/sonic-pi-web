262807af38991ee57d674e1307f8ec08aaa63a1e
Bruno Maglioni <br1@br1-debian.ipv6.infinitum.net.mx>
2020-04-20 12:31:08 -0500

    list, add and delete pads thorugh ajax

--- public/javascripts/pads-index.js
+++ public/javascripts/pads-index.js
@@ -1,6 +1,79 @@
 $(document).ready(function() {
-    $('#create').click(function() {
-        let padName = $('#padName').val();
-        $.notify('Not ready, yet', 'warning');
-    });
+	listPads();
+
+	$('#create').click(function() {
+		let padID = $('#newPadID').val();
+		createPad(padID);
+	});
+
+	// delegate to the table as appended/re-appended elements lose the binding to OnClick
+	$('#padList').delegate('.delete', "click", function(){
+		let id = $(this).attr('id'); // get id of button clicked
+		let split = id.split('.'); // split in substrings [format -> id.delete]
+		let padID = split[0]; // first part is the pad id
+		
+		deletePad(padID);
+	});
+		
 });
+
+// list all ether pads
+function listPads() {
+	$.get('/pads', function(res) {
+		let pads = res.padIDs;
+
+		// open table body
+		let tableHTML = '<tbody>';
+		$('#padList').append(tableHTML);
+
+		pads.forEach(function(pad) {
+			let btnJoin = '<a class="button" id="' + pad + '.join" href="/pads/join/' + pad + '">join</a>';
+			let btnDelete = '<button class="button button-danger delete" id="' + pad + '.delete">delete</button>';
+			let row = '<tr><td>' + pad + '</td><td>' + btnJoin + btnDelete + '</td></tr>';
+			
+			$('#padList').append(row);
+		});
+
+		$('#padList').append('</tbody>'); // close table body
+	})
+	/*.done(function() {
+		$.notify('Pad list retrieved', 'info');
+	})*/
+	.fail(function(error) {
+		$.notify('Error getting the pad list: ' + error.responseJSON.message, 'error');
+	});
+}
+
+// update (redraw) table
+function updateTable() {
+	$("#padList tbody").empty();
+	listPads();
+}
+
+// create a new pad (groupless, for now)
+function createPad(padID) {
+	$.get('/pads/new/' + padID, function(res) {
+		//console.log(res);
+	})
+	.done(function() {
+		$.notify('Pad created', 'success');
+		updateTable();
+	})
+	.fail(function(error) {
+		$.notify('Error creating pad: ' + error.responseJSON.message, 'error');
+	});
+}
+
+// delete a registered pad
+function deletePad(padID) {
+	$.get('/pads/delete/' + padID, function(res) {
+		//console.log(res);
+	})
+	.done(function() {
+		$.notify('Pad deleted', 'success');
+		updateTable();
+	})
+	.fail(function(error) {
+		$.notify('Error deleting pad: ' + error.responseJSON.message, 'error');
+	});
+}
