c522c3facec6dfe29e0f038787948a277480016c
Bruno Maglioni <br1@br1-debian.ipv6.infinitum.net.mx>
2020-04-20 22:58:21 -0500

    username test

--- public/javascripts/pad.js
+++ public/javascripts/pad.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var padID = $('#padID').text();
-    $('#epad').pad({'padId':padID});
+    $('#epad').pad({'padId': padID, 'userName': 'uname'});
 
     $('#run').click(function() {
         $.notify('Not ready, yet', 'warning');
