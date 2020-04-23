64e8bd625482d65b23113635a163d7a7a865572e
Bruno Maglioni <br1@br1-debian.ipv6.infinitum.net.mx>
2020-04-23 13:12:34 -0500

    welcome notification

--- public/javascripts/pad.js
+++ public/javascripts/pad.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
     var padID = $('#padID').text();
     $('#epad').pad({'padId': padID, 'userName': 'uname'});
 
+    $.notify('// Welcome to: ' + padID, {
+      	style: 'transparent',
+      	className: 'success' 
+    });
+
     $('#run').click(function() {
         $.notify('Not ready, yet', 'warning');
     });
