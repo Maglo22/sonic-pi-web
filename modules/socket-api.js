c63a5f6454d49a2d24ca6a69e2812713489025ac
Bruno Maglioni <br1@br1-debian.ipv6.infinitum.net.mx>
2020-04-26 11:52:55 -0500

    getting the text of a pad and saving it to file

--- modules/socket-api.js
+++ modules/socket-api.js
@@ -1,10 +1,44 @@
 var socket_io = require('socket.io');
+var etherpad_api = require('../modules/etherpad-api');
 var io = socket_io();
 var socket_api = {};
+var fs = require('fs'); // file system
+
+const { exec } = require("child_process");
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  //console.log('a user connected');
+  socket.on('run pad', (padID) => {
+    var args = {
+      padID: padID
+    }
+    etherpad_api.getText(args, (error, data) => {
+      if (error) {
+        console.error(error);
+      } else {
+        fs.writeFile('/home/br1/test-files/test.txt', data.text, function (err) { // TODO: change to relative path
+          if (err) return console.log(err);
+          console.log('text > test.txt');
+        });
+      }
+    });
+  });
+
 });
+
+function cmd(command) {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`success/nstdout: ${stdout}`);
+  });
+}
 
 
 socket_api.io = io;
