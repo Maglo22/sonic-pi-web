d6123d0a4cd933ef4861a07c7fba3858d78e176b
Bruno Maglioni <br1@br1-debian.ipv6.infinitum.net.mx>
2020-05-12 15:49:20 -0500

    make directory for pad files relative

--- modules/socket-api.js
+++ modules/socket-api.js
@@ -1,57 +1,77 @@
 var socket_io = require('socket.io');
-var etherpad_api = require('../modules/etherpad-api');
 var io = socket_io();
 var socket_api = {};
+
+var etherpad_api = require('../modules/etherpad-api');
+var path = require('path');
 var fs = require('fs'); // file system
 
-const { exec } = require("child_process");
+const { exec } = require('child_process');
 
-var path = '/home/br1/test-files/'; // TODO: make it relative
+var dir = path.join(__dirname, '..', 'pad_files/');
+
 
 io.on('connection', (socket) => {
   // play content on pad
   socket.on('run pad', (padID) => {
-    var args = {
-      padID: padID
-    }
+    var args = { padID: padID }
+
     etherpad_api.getText(args, (error, data) => {
       if (error) {
         console.error(error);
       } else {
         let fileName = padID + '.rb';
-        let pathToFile = path + fileName;
+        let pathToFile = dir + fileName;
         
-        saveToFileAndRun(pathToFile, data.text);
+        dirCheck(padID, pathToFile, data.text); // check if the directory for pad files exist before playing the file
       }
     });
   });
 
   // stop playing pad
   socket.on('stop pad', (padID) => {
+    console.log('stopping pad: // ' + padID);
     runCommand('sonic-pi-tool stop');
   });
 
   // notification for users in pad
-  socket.on('notify', (msg, style, classname) => {
-    io.emit('notify', msg, style, classname);
-  });
+  socket.on('notify', (msg, style, classname) => { io.emit('notify', msg, style, classname); });
 
 });
 
+// check if directory for saving files is found
+function dirCheck(padID, pathToFile, text) {
+  fs.access(dir, (err) => {
+    if (err) {
+      // dir not found
+      if (err.code === 'ENOENT') {
+        fs.mkdir(dir, (err) => { 
+          if (err) throw err;
+
+          console.log('pad_files dir not found, creating it');
+          console.log('playing pad: // ' + padID);
+          saveFileAndRun(pathToFile, text); // save file and play it with sonic-pi-tool
+        }); 
+      } else { throw err; }
+    }
+    // dir found
+    console.log('playing pad: // ' + padID);
+    saveFileAndRun(pathToFile, text); // save file and play it with sonic-pi-tool
+  });
+}
+
 // save text of the pad to file for sonic-pi-tool to run
-function saveToFileAndRun(pathToFile, text) {
+function saveFileAndRun(pathToFile, text) {
   fs.open(pathToFile, 'w', (err, fd) => {
     if (err) throw err;
-    fs.write(fd, text, (err, written, string) => {
+    fs.write(fd, text, (err, written) => {
       if (err) throw err;
-      console.log('written ' + written + ' bytes to ' + pathToFile);
 
+      console.log('written ' + written + ' bytes to ' + pathToFile);
       runCommand('sonic-pi-tool eval-file ' + pathToFile); // run the file with sonic-pi-tool
   
       // close file descriptor
-      fs.close(fd, (err) => {
-        if (err) throw err;
-      });
+      fs.close(fd, (err) => { if (err) throw err; });
     });
   });
 }
